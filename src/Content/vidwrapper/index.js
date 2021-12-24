a10af0d9d6fe5804e9686344d9bf225986597e6a
nguyen0van0b <44321049+nguyen0van0b@users.noreply.github.com>
2021-12-24 10:24:20 +0700

    update banner

--- src/Content/vidwrapper/index.js
+++ src/Content/vidwrapper/index.js
@@ -1,12 +1,54 @@
+import { useEffect } from 'react'
 import urlvideo from '../../media/metagunner_clip_banner.mp4'
-import urlimg from '../../media/metagunner_banner.png'
+import urlimg1 from '../../media/layer1.png'
+import urlimg2 from '../../media/layer2.png'
+import urlimg3 from '../../media/layer3.png'
+import urlimg4 from '../../media/layer4.png'
+import urlimgmb from '../../media/metagunner_banner_500.png'
 import style from './vidwrapper.module.scss'
 
 function Vidwrapper(){
+    useEffect(()=>{
+    document.addEventListener("mousemove", parallax);
+    function parallax(e) {
+        document.querySelectorAll(".image1").forEach(function(move) {
+            var moving = move.getAttribute("data-value");
+            var x = e.clientX * -moving / 200;
+            var y = e.clientY * -moving / 200;
+            move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+        });
+        document.querySelectorAll(".image2").forEach(function(move) {
+            var moving = move.getAttribute("data-value");
+            var x = e.clientX * -moving / 200;
+            var y = e.clientY * -moving / 200;
+            move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+        });
+        document.querySelectorAll(".image3").forEach(function(move) {
+            var moving = move.getAttribute("data-value");
+            var x = e.clientX * -moving / 100;
+            var y = e.clientY * -moving / 100;
+            move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+        });
+        document.querySelectorAll(".image4").forEach(function(move) {
+            var moving = move.getAttribute("data-value");
+            var x = e.clientX * -moving / 150;
+            var y = e.clientY * -moving / 150;
+            move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+        });
+    }
+    });
     return(
         <section className={style.Vidwrapper}>
-            <video src={urlvideo} autoplay='autoplay' muted='muted' loop='loop'></video>
-            <img src={urlimg} alt=""/>
+            {/* <video src={urlvideo} autoplay='autoplay' muted='muted' loop='loop' className='image1'></video> */}
+            <div className={style.banner_PC}>
+            <img src={urlimg1} className='image1' data-value="5" alt="" style={{width:'120%'}}/>
+            <img src={urlimg2} className='image2' data-value="5" alt=""  style={{width:'110%'}}/>
+            <img src={urlimg3} className='image3' data-value="5" alt=""  style={{width:'100%'}}/>
+            <img src={urlimg4} className='image4' data-value="5" alt="" style={{width:'100%'}}/>
+            </div>
+            <div className={style.banner_MB}>
+            <img src={urlimgmb} alt="" />
+            </div>
         </section>
     )
 }
