3df9657b29049ff57fe83ade60ae8345c771a827
nguyen0van0b <44321049+nguyen0van0b@users.noreply.github.com>
2021-12-30 17:28:15 +0700

    update background

--- src/Content/basecamp/index.js
+++ src/Content/basecamp/index.js
@@ -14,7 +14,7 @@ function Basecamp(){
            </div>
             <video src={urlVideo} autoplay='autoplay' muted='muted' loop='loop'></video>
             <div className={style.basecampTitleP}>
-                <p>Basecamp is the place for soldiers to rest and heal after fighting in fierce battles. Basecamp owners can get more $MGG without going into monster fights, reduce ammo recovery time for weapons, and have more chance to receive items such as Weapon Cards, Character Cards... </p>
+                <p>Basecamp is the place for soldiers to rest and heal after fighting in fierce battles. Basecamp owners can get more rewards without going into monster fights, reduce ammo recovery time for weapons, and have more chance to receive items such as Weapon Cards, Character Cards... </p>
             </div>
         </section>
     )
