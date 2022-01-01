afb8e82b76458530719d7cbe90c54bfdaa3712ff
nguyen0van0b <44321049+nguyen0van0b@users.noreply.github.com>
2022-01-01 14:38:53 +0700

    update

--- src/footer/index.js
+++ src/footer/index.js
@@ -11,9 +11,10 @@ import style from './footer.module.scss'
 function Footer(){
     return(
         <footer className={style.footer}>
-        <div className="container">
             <div className={style.footerWrapper}>
+                <div className={style.footerWrapperLogo}>
                 <img src={imgLogo} alt=""/>
+                </div>
                 {/* <p>The first Racing Game on BSC Deathroad aims to become a comprehensive gaming ecosystem built for next generation racers, who utilize racing skills to enjoy the real racing expericence and to earn tradable valuable digital assets.</p> */}
                 <div className={style.footerWrapperSocial}>
                     <a href="https://t.me/MetaGunnerChannel" target="_blank" rel="noopener noreferrer"><img src={imgTelegram} alt=""/></a>
@@ -30,7 +31,26 @@ function Footer(){
                 </ul> */}
                 <p className={style.copyright}>Copyright © 2021 MetaGunner All rights reserved.</p>
             </div>
-        </div>
+            <div className={style.footerMenu}>
+                <div className={style.footerMenuAbout}>
+                    <h2>ABOUT</h2>
+                    <ul>
+                        <li>FEATURES</li>
+                        <li>CHARACTER</li>
+                        <li>ROADMAP</li>
+                        <li>TOKENOMICS</li>
+                        <li>TEAM</li>
+                    </ul>
+                </div>
+                <div className={style.footerMenuFAQ}>
+                    <h2>FAQ</h2>
+                    <ul>
+                        <li><a href="https://whitepaper.metagunner.io/game-features/disclaimer" target="_blank">Disclaimer</a></li>
+                        <li><a href="https://whitepaper.metagunner.io/" target="_blank">Whitepaper</a></li>
+                        <li><a href="mailto:contact@metagunner.io">Contact@metagunner.io</a></li>
+                    </ul>
+                </div>
+            </div>
     </footer>
     )
 }
