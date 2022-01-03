9cea7e842ed88a813fb14e9dcd9aae7a7ba30d1b
nguyen0van0b <44321049+nguyen0van0b@users.noreply.github.com>
2022-01-03 16:23:23 +0700

    update characte & basecamp mobile

---
+++ src/Content/characterMB/index.js
@@ -0,0 +1,111 @@
+import url1 from "../../media/Gunner.png"
+import url2 from "../../media/Scout.png"
+import url3 from "../../media/Sniper.png"
+import url4 from "../../media/special-force.png"
+import url5 from "../../media/destroyer.png"
+import url6 from "../../media/Medic.png"
+import url7 from "../../media/Officer.png"
+import urlCHARACTER from "../../media/CHARACTER.png"
+import clsx from "clsx"
+import React, { Component } from "react";
+import Slider from "react-slick";
+import style from "./characterMB.module.scss"
+import "../../../node_modules/slick-carousel/slick/slick.css"; 
+import "../../../node_modules/slick-carousel/slick/slick-theme.css";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{...style,position: 'absolute',top: '50%',right: 0,width: '11%',height: '100%'}}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{...style,position: 'absolute',top: '50%',left: 0,zIndex:1,width: '11%',height: '100%'}}
+      onClick={onClick}
+    />
+  );
+}
+export default class AsNavFor extends Component {
+  render() {
+    const settings1 = {
+      dots: true,
+      infinite:true,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />
+    };
+    return (
+      <section className={style.character} id="character">
+        <div className={clsx('container',style.characterContainer)}>
+          <div className={style.charactertitle}>
+            <img src={urlCHARACTER} alt=""/>
+            <p>The 15 characters of MetaGunner are divided into seven types of troops</p>
+          </div>
+        </div>
+        <Slider
+          ref={slider => (this.slider1 = slider)}
+          {...settings1}
+        >
+          <div className={style.characterSecondSlider}>
+                <div className={style.characterSecondSliderTitle}>
+                    <h2>Gunner</h2>
+                    <span>A well-trained Navy SEAL that can improvise, adapt, and overcome any situations. He is a real weapon-savvy and can make the most of everything around him to finish a job</span>
+                </div>
+                <img src={url1} alt=""/>
+          </div>
+          <div className={style.characterSecondSlider}>
+                <div className={style.characterSecondSliderTitle}>
+                    <h2>Scout</h2>
+                    <span>The scout is an expert on gathering information on the enemy lands. He is a master of army-styled ninjutsu who always come with a plan and always leave a place with many dead bodies</span>
+                </div>
+                <img src={url2} alt=""/>
+          </div>
+          <div className={style.characterSecondSlider}>
+                <div className={style.characterSecondSliderTitle}>
+                    <h2>Sniper</h2>
+                    <span>No one has ever seen him, and no one wants to be his enemy because you will never know when he's gonna get you. His targets never survive, but the witnesses said there was a whistle before the kill.</span>
+                </div>
+                <img src={url3} alt=""/>
+          </div>
+          <div className={style.characterSecondSlider}>
+                <div className={style.characterSecondSliderTitle}>
+                    <h2>Special Force</h2>
+                    <span>An elite trooper specializes in large-scale tasks and always complete them thoroughly. He can deliver every methods that you can possibly think of to kill a man</span>
+                </div>
+                <img src={url4} alt=""/>
+          </div>
+          <div className={style.characterSecondSlider}>
+                <div className={style.characterSecondSliderTitle}>
+                <h2>Destroyer</h2>
+                    <span>The Destroyer is a heavy-built six-foot tall man who loves big guns and explosives. His enemies may outrun him, but they cannot outrun his grenades</span>
+                </div>
+                <img src={url5} alt=""/>
+          </div>
+          
+          <div className={style.characterSecondSlider}>
+                <div className={style.characterSecondSliderTitle}>
+                    <h2>Medic</h2>
+                    <span>He is an extremely experienced battlefield doctor, but do not make the mistake of underestimate his skills. He brings people back from the brink of death, yet he also does a perfect job of sending one to hell.</span>
+                </div>
+                <img src={url6} alt=""/>
+          </div>
+          <div className={style.characterSecondSlider}>
+                <div className={style.characterSecondSliderTitle}>
+                    <h2>Officer</h2>
+                    <span>A respectful but ruthless soldier that hold a record of bring 21 terrorists to justice by himself</span>
+                </div>
+                <img src={url7} alt=""/>
+          </div>
+        </Slider>
+      </section>
+    );
+  }
+}
