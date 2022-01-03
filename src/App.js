6476d2937aa337baa011d4b09be6154664040da2
nguyen0van0b <44321049+nguyen0van0b@users.noreply.github.com>
2022-01-03 15:32:38 +0700

    update roadmap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './header'
 import {Vidwrapper,About,Features,Character,Basecamp,Roadmap,Tokenomics,Communities,Partners,Investors,TEAM,NFTsItem} from './Content'
 import Footer from './footer'
 import GlobalStyles from './GlobalStyles'
+import logo from './media/logo-200x200.png'
 
 function App() {
   return (
